08ec1ca6a9013007c6ad380c87e614a0075279a8
jspickerman <jonathanspickerman@gmail.com>
2021-01-19 14:00:54 -0600

    1 file modified

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+test();
+
+const waitFiveHundred = async () => {
+  console.log('500 start!');
+  setTimeout(() => {
+    console.log('500 passed!');
+  }, 500)
+}
+
+function test() {
+  console.log('yeet!');
+  waitFiveHundred.then(console.log('done!'))
+}
