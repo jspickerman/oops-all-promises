928b11a89f8e4a74464592411cf62d46c26f5e65
jspickerman <jonathanspickerman@gmail.com>
2021-01-19 14:34:25 -0600

    1 file modified

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 const waitFor = (delay) => {
   console.log('waiting for ' + delay + ' ms');
-  return new Promise(resolve => setTimeout(resolve, delay))
+  return new Promise(resolve => setTimeout(resolve(delay), delay))
 };
 
 async function test() {
   console.log('yeet!');
-  waitFor(5000).then(() => console.log('done!'));
+  waitFor(5000).then((data) => console.log(data));
   const holdUp = await waitFor(7500);
   console.log(holdUp);
 }
